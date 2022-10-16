cc42e3c974c949b4715fa2f4c527a8fc44759ad3
constantcmu <constant.eng.cmu@gmail.com>
2022-10-16 22:08:58 +0700

    up

---
+++ app_dashboard.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+
+st.title("Lit Finance Dashboard")
+tickers = ("TSLA",'AAPL','BTC-USD','ETH-USD',"BNB-USD","ADA-USD","goog", "amzn", "BAC", "BA")
+
+dropdown = st.multiselect('Pick your assets',
+                        tickers)
+start = st.date_input('Start',value=pd.to_datetime('2020-01-01'))
+end = st.date_input("End",value=pd.to_datetime('today'))
+
+
+def relativeret(df):
+    rel = df.pct_change()
+    cumret = (1+rel).cumprod() - 1
+    cumert = cumret.fillna(0)
+    return cumert
+
+if len(dropdown)>0:
+    # df = yf.download(dropdown,start,end)["Adj Close"]
+    df = relativeret(yf.download(dropdown,start,end)["Adj Close"])
+    
+    st.line_chart(df)
+
+
+
+
